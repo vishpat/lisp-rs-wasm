7b25be36d21fbb7d7df9d98ea17b79eb86de9ac4
Vishal Patil <vishpat@gmail.com>
2022-05-29 17:19:21 -0400

    Able to get result

--- src/App.js
+++ src/App.js
@@ -4,24 +4,38 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [result, setResult] = useState();
+  const [input, setInput] = useState('');
+
   useEffect(() => {
     init().then(() => {
-      let r = eval_lisp_prg('(+ 1 8)');
-      console.log("UseEffect: Setting result", r);
-      setResult(r);
+      if (input) {
+        let r = eval_lisp_prg(input);
+        console.log("UseEffect: Setting result", r);
+        setResult(r);
+      }
     }) 
   }, []);
 
   const getResult = () => {
+    
     init().then(() => {
-      let r = eval_lisp_prg("(+ 1 2)");
-      console.log("Get Result: Setting result", r);
+      console.log("Button Click: Getting result for", input);
+      let r = eval_lisp_prg(input);
+      console.log("Button Click: Setting result to", r);
       setResult(r);
     });
   };
+
+  const updateInput = (e) => {
+    setInput(e.target.value);
+    console.log("Input: Setting input", e.target.value);
+  };
+
   return (
     <div className="App">
+      <textarea onChange={updateInput}/>
       <h4> {result}</h4>
+    <button onClick={getResult}>Get Result</button>       
     </div>
   );
 }
