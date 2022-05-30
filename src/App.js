3ad4e186fe939402f7e4b2fc8fc339a343096753
Vishal Patil <vishpat@gmail.com>
2022-05-29 22:51:14 -0400

    Fixed formatting

--- src/App.js
+++ src/App.js
@@ -43,18 +43,22 @@ function App() {
     <div className="App">
       <div className="container">
         <h1>Lisp Interpreter in a browser using WASM</h1>
-        <p>This webpage runs a <a href="https://webassembly.org/"> WASM </a> Lisp Interpreter in the browser. 
-        The interpreter was written in <a href="https://www.rust-lang.org">Rust</a> and it's 
-          implementation can found <a href="http://github.com/vishpat/lisp-rs">here.</a></p> 
-          You can run some of the Lisp programs using the textbox below. Information 
-          regarding the syntax and some sample programs can be found <a href="https://github.com/vishpat/lisp-rs/wiki/Lisp-Syntax"> here</a>
+        <div className="alert alert-secondary" role="alert">
+        <p align="left">This webpage hosts a Lisp Interpreter in a browser. 
+        The interpreter was written in <a href="https://www.rust-lang.org">Rust</a> and compiled to <a href="https://webassembly.org/"> WASM </a>.
+          The implementation of the interpreter can found <a href="http://github.com/vishpat/lisp-rs">here.</a> 
+          The source code for the WASM bindings and this webapp can be found <a href="https://github.com/vishpat/lisp-rs-wasm">here.</a>  
+          You can run the Lisp programs by typing in the code in the textbox below. Information 
+          regarding the syntax and some sample programs for the Lisp dialect can be found <a href="https://github.com/vishpat/lisp-rs/wiki/Lisp-Syntax"> here</a>
+        </p>
+        </div>
       </div>
       <div className="container">
         <textarea rows="10" columns="20" className="form-control" onChange={updateprogram} />
       </div>
       <div className="container-fluid">
         <h4> {result}</h4>
-        <button className="btn btn-primary" onClick={getResult}>Get Result</button>
+        <button className="btn btn-primary" onClick={getResult}>Run</button>
       </div>
 
     </div>
